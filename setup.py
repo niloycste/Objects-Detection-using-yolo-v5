59edc2e521a96a0000fde6ab2625b4d34a373b92
ni6670154@gmail.com <ni6670154@gmail.com>
2024-01-12 18:43:43 +0600

    set

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'wasteDetection',
+    version= '0.0.0',
+    author= 'M Mohaiminul Islam',
+    author_email= 'niloy8649@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
